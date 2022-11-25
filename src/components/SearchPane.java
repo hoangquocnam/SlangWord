16a5171d757b8ac48cf8c96d5c44caac5165909f
Bun <hoangquocnam021117@gmail.com>
2022-11-25 22:17:39 +0700

    fix: Add search button

--- src/components/SearchPane.java
+++ src/components/SearchPane.java
@@ -1,6 +1,7 @@
 package components;
 
 import java.awt.*;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -12,8 +13,9 @@ import javax.swing.border.TitledBorder;
 public class SearchPane extends JPanel {
 
   JComboBox<String> SearchTypeBox;
+  JButton SearchButton;
+  static String[] SEARCH_TYPES = { "Definition", "Slang", "All" };
 
-  String[] SEARCH_TYPES = { "Definition", "Slang" , "All" };
   public void prepareTypeBox() {
     SearchTypeBox = new JComboBox<String>(SEARCH_TYPES);
   }
@@ -22,6 +24,8 @@ public class SearchPane extends JPanel {
     prepareTypeBox();
     setLayout(new GridBagLayout());
     GridBagConstraints gbc = new GridBagConstraints();
+
+    // KEYWORD
     gbc.gridx = 0;
     gbc.gridy = 0;
     add(new JLabel("Search for:"), gbc);
@@ -30,16 +34,27 @@ public class SearchPane extends JPanel {
     gbc.gridy = 0;
     add(new JTextField(50), gbc);
 
-    gbc.gridy = 2;
+    //SELECT SEARCH TYPE
+    gbc.gridy = 1;
     gbc.gridx = 0;
     gbc.anchor = GridBagConstraints.EAST;
     gbc.weightx = 0;
     gbc.fill = GridBagConstraints.NONE;
     add(new JLabel("By:"), gbc);
+
     gbc.anchor = GridBagConstraints.WEST;
     gbc.gridx++;
     gbc.weightx = 1;
     add(SearchTypeBox, gbc);
+
+    //BUTTON
+    SearchButton = new JButton("Search");
+    gbc.gridy = 3;
+    gbc.gridx = 2;
+    gbc.anchor = GridBagConstraints.EAST;
+    gbc.weightx = 1;
+    add(SearchButton, gbc);
+
     setBorder(
       new CompoundBorder(
         new TitledBorder("SEARCH"),
